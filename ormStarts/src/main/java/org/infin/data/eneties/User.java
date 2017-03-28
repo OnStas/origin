ee3ca9037296221a2f8b1b3cf7b192ab1340aaa6
Stanislav <kate@kate-PC>
2017-03-28 18:48:39 +0300

    user db mapping

---
+++ ormStarts/src/main/java/org/infin/data/eneties/User.java
@@ -0,0 +1,74 @@
+package org.infin.data.eneties;
+
+import java.util.Date;
+
+public class User {
+
+	private Long userId;
+	private String firstName;
+	private String lastName;
+	private Date birthDate;
+	
+	
+	
+	public Long getUserId() {
+		return userId;
+	}
+	public void setUserId(Long userId) {
+		this.userId = userId;
+	}
+	public String getFirstName() {
+		return firstName;
+	}
+	public void setFirstName(String firstName) {
+		this.firstName = firstName;
+	}
+	public String getLastName() {
+		return lastName;
+	}
+	public void setLastName(String lastName) {
+		this.lastName = lastName;
+	}
+	public Date getBirthDate() {
+		return birthDate;
+	}
+	public void setBirthDate(Date birthDate) {
+		this.birthDate = birthDate;
+	}
+	public String getEmailAddress() {
+		return emailAddress;
+	}
+	public void setEmailAddress(String emailAddress) {
+		this.emailAddress = emailAddress;
+	}
+	public Date getLastUpdateDate() {
+		return lastUpdateDate;
+	}
+	public void setLastUpdateDate(Date lastUpdateDate) {
+		this.lastUpdateDate = lastUpdateDate;
+	}
+	public String getLastUpdateBy() {
+		return lastUpdateBy;
+	}
+	public void setLastUpdateBy(String lastUpdateBy) {
+		this.lastUpdateBy = lastUpdateBy;
+	}
+	public Date getCreateDate() {
+		return createDate;
+	}
+	public void setCreateDate(Date createDate) {
+		this.createDate = createDate;
+	}
+	public String getCteatedBy() {
+		return cteatedBy;
+	}
+	public void setCteatedBy(String cteatedBy) {
+		this.cteatedBy = cteatedBy;
+	}
+	private String emailAddress;
+	private Date lastUpdateDate;
+	private String lastUpdateBy;
+	private Date createDate;
+	private String cteatedBy;
+	
+}
