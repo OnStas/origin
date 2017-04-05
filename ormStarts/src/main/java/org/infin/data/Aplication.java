f0450fe079061b9e675daa03c5e18795047f195d
Stanislav <kate@kate-PC>
2017-04-05 10:54:36 +0300

    now its injects  commit method working good

--- ormStarts/src/main/java/org/infin/data/Aplication.java
+++ ormStarts/src/main/java/org/infin/data/Aplication.java
@@ -1,6 +1,9 @@
 package org.infin.data;
 
+import java.util.Date;
+
 import org.hibernate.Session;
+import org.infin.data.eneties.User;
 
 public class Aplication {
 
@@ -8,6 +11,21 @@ public class Aplication {
 			
 		Session session = HibernateUtil.getSessionFactory().openSession();
 		session.beginTransaction();
+		
+
+		User user = new User();
+		user.setBirthDate(new Date());
+		user.setCreatedBy("kevinBoo");
+		user.setCreatedDate(new Date());
+		user.setEmailAddress("kmb385@yahoo.com");
+		user.setFirstName("Kevin");
+		user.setLastName("Bowersox");
+		user.setLastUpdatedBy("kevin");
+		user.setLastUpdatedDate(new Date());
+		
+		session.save(user);
+		
+		session.getTransaction().commit();
 		session.close();
 		
 	}
--- ormStarts/src/main/java/org/infin/data/eneties/User.java
+++ ormStarts/src/main/java/org/infin/data/eneties/User.java
@@ -2,73 +2,108 @@ package org.infin.data.eneties;
 
 import java.util.Date;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="FINANCES_USER")
 public class User {
 
 	private Long userId;
 	private String firstName;
 	private String lastName;
 	private Date birthDate;
-	
-	
-	
+	private String emailAddress;
+	private Date lastUpdatedDate;
+	private String lastUpdatedBy;
+	private Date createdDate;
+	private String createdBy;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(name = "USER_ID")
 	public Long getUserId() {
 		return userId;
 	}
+
 	public void setUserId(Long userId) {
 		this.userId = userId;
 	}
+
+	@Column(name = "FIRST_NAME")
 	public String getFirstName() {
 		return firstName;
 	}
+
 	public void setFirstName(String firstName) {
 		this.firstName = firstName;
 	}
+
+	@Column(name = "LAST_NAME")
 	public String getLastName() {
 		return lastName;
 	}
+
 	public void setLastName(String lastName) {
 		this.lastName = lastName;
 	}
+
+	@Column(name = "BIRTH_DATE")
 	public Date getBirthDate() {
 		return birthDate;
 	}
+
 	public void setBirthDate(Date birthDate) {
 		this.birthDate = birthDate;
 	}
+
+	@Column(name = "EMAIL_ADDRESS")
 	public String getEmailAddress() {
 		return emailAddress;
 	}
+
 	public void setEmailAddress(String emailAddress) {
 		this.emailAddress = emailAddress;
 	}
-	public Date getLastUpdateDate() {
-		return lastUpdateDate;
+
+	@Column(name = "LAST_UPDATED_DATE")
+	public Date getLastUpdatedDate() {
+		return lastUpdatedDate;
 	}
-	public void setLastUpdateDate(Date lastUpdateDate) {
-		this.lastUpdateDate = lastUpdateDate;
+
+	public void setLastUpdatedDate(Date lastUpdatedDate) {
+		this.lastUpdatedDate = lastUpdatedDate;
 	}
-	public String getLastUpdateBy() {
-		return lastUpdateBy;
+
+	@Column(name = "LAST_UPDATED_BY")
+	public String getLastUpdatedBy() {
+		return lastUpdatedBy;
 	}
-	public void setLastUpdateBy(String lastUpdateBy) {
-		this.lastUpdateBy = lastUpdateBy;
+
+	public void setLastUpdatedBy(String lastUpdatedBy) {
+		this.lastUpdatedBy = lastUpdatedBy;
 	}
-	public Date getCreateDate() {
-		return createDate;
+
+	@Column(name = "CREATED_DATE")
+	public Date getCreatedDate() {
+		return createdDate;
 	}
-	public void setCreateDate(Date createDate) {
-		this.createDate = createDate;
+
+	public void setCreatedDate(Date createdDate) {
+		this.createdDate = createdDate;
 	}
-	public String getCteatedBy() {
-		return cteatedBy;
+
+	@Column(name = "CREATED_BY")
+	public String getCreatedBy() {
+		return createdBy;
 	}
-	public void setCteatedBy(String cteatedBy) {
-		this.cteatedBy = cteatedBy;
+
+	public void setCreatedBy(String createdBy) {
+		this.createdBy = createdBy;
 	}
-	private String emailAddress;
-	private Date lastUpdateDate;
-	private String lastUpdateBy;
-	private Date createDate;
-	private String cteatedBy;
-	
+
 }
